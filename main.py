77aa790c3c7ad58890fc59f43c7f239d3a07a349
Starodubtsev_m <Starodubtsevm@gmail.com>
2020-02-23 20:40:20 +0500

    первый релиз. все работает. все упрятано в класс

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from classes import *
+from config import *
+
+#---------------------------------------------------------------------------
+#afc1 = afc_device(amplitude,frequency,blocksize,samplerate,
+#	freq_min,freq_max,freq_step,time_conv)
+afc1 = afc_device()
+
+input("Press Enter to exit")
+#---------------------------------------------------------------------------
